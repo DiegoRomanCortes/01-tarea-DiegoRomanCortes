57cf7cd59706f1ddcb2e50342eb2a2cd101fa5d0
Diego Román Cortés <48524687+DiegoRomanCortes@users.noreply.github.com>
2020-09-14 22:57:55 -0300

    codigo funcional, falta avanzar en el informe

--- Tarea1/Codigo/prob.py
+++ Tarea1/Codigo/prob.py
@@ -1,52 +1,71 @@
 import numpy as np
-import scipy.special as sci
 
-k = 4.495
-
-def pto_medio(func, a, b):
+def pto_medio(func, z, a, b):
     dx = b-a
     med = (a+b)/2
-    rec = func(med)
+    rec = func(z, med)
     return rec*dx
 
-def simpson(func, a, b, N):
-    dx = (b-a)/(2*N)
-    f0 = func(a)
-    par = 0
-    impar = 0
-    f2N = func(b)
+def trapecio(func, z, a, b, N = 100):
+    dx = (b-a)/(N)
+    f0 = func(z, a)
+    fN = func(z, b)
 
-    for i in range(0,N):
-        par += func(a+2*i*dx)
-        impar += func(a+(2*i+1)*dx)
-
-    sum = f0 + 2*par + 4*impar + f2N
-    sum = sum*dx/3
+    sum = (f0 + fN)/2
+    for i in range(1,N):
+        sum += func(z, a+i*dx)
+    sum *= dx
     return sum
 
-def Gamma(x):
-    return sci.gamma(x) #mientras se me ocurre el cambio de variable apropiado
 
-def chi2(x):
-    res = 1/(np.exp2(k/2)*Gamma(k/2))*x**(k/2 - 1)*np.exp(-x/2) #definicion de chi^2(x)
+def simpson(func, z, a, b, N = 100, rel_tol = 1e-6):
+    sum = trapecio(func, z, a, b, N)    
+    sum2 = trapecio(func, z, a, b, 2*N)
+    simp = (4*sum2 - sum)/3
+    dif = np.abs((simp - sum)/sum)
+    if dif < rel_tol:
+        return sum
+    else:
+        return simpson(func, z, a, b, 2*N, rel_tol)
+
+def Gamma_du(z, u): #integando de la funcion gamma con u=e^(-x)
+    gdu = np.log(1/u)**(z-1)
+    return gdu
+
+def Gamma(z, dx = 1e-7):
+    #nuevos extremos gracias al C.V.
+    a = 0
+    b = 1
+    g = simpson(Gamma_du, z, a + dx, b)
+    g += pto_medio(Gamma_du, z, a, a + dx) 
+    return g
+
+K = 4.495 #rut 20299495-4
+gamma_k_medios = Gamma(K/2)
+
+def chi2(k, x):
+    res = 1/(np.exp2(k/2)*gamma_k_medios)*x**(k/2 - 1)*np.exp(-x/2) #definicion de chi^2(x)
     return res
 
 #calculo de int_0^a chi^2(x) dx, x<0 no es parte del dominio de chi^2
-def prob_chi2(a, N=1000):
-    prob = simpson(chi2, 0, a, N)
+def prob_chi2(k, a):
+    prob = simpson(chi2, k, 0, a)
     return prob
 
 #el problema prob_chi2(a) = 0.95 se puede reescribir como el problema del cero de una funcion
 # definiendo g(a) = prob_chi2(a) - 0.95 = 0
-def biseccion(func, a, b, tol=1e-7, lim=1000):
-    p = (a+b)/2
+def biseccion(func, k, a, b, tol=1e-6, lim=1000):
     counter = 0
-    while np.abs(func(b)-func(a)) > tol:
+    dif = np.abs(func(k, b)-func(k, a))
+    while  dif > tol:
+        dif = np.abs(func(k, b)-func(k, a))
         p = (a+b)/2
         if counter > lim: 
             print('limite de iteraciones excedido')
             return p
-        prod = func(p)*func(a)
+        fp = func(k, p)
+        fa = func(k, a)
+        prod = fp*fa
         
         if prod > 0:
             a = p
@@ -55,8 +74,13 @@ def biseccion(func, a, b, tol=1e-7, lim=1000):
         counter += 1
     return p
 
-def prob_menos_95(a):
-    res = prob_chi2(a) - 0.95
+def prob_menos_95(k, a):
+    res = prob_chi2(k, a) - 0.95 #se quiere encontrar res = 0
     return res
 
-a = biseccion(prob_menos_95, 5, 15) #el valor debe ser cercano a 10.3
+#print(Gamma(2))
+#print(Gamma(3)) #Gamma(n) = (n-1)!
+                #como k/2 es cercano a 2 se han ajustado los paramentros para que gamma(2) aprox 1
+
+a = biseccion(prob_menos_95, K, 5, 15) #a debiera ser cercano a 10.28
+print("El valor de a es:", a)
