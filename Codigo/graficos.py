24719e6418397873a77a900b24d3e5fdf94e295b
Diego Román Cortés <48524687+DiegoRomanCortes@users.noreply.github.com>
2020-09-22 14:36:37 -0300

    Separa graficos de soluciones. Agrega Newton.

---
+++ Codigo/graficos.py
@@ -0,0 +1,135 @@
+from prob import *
+import matplotlib.pyplot as plt
+
+#codigo para graficar el integrando original de Gamma
+x = np.linspace(0, 10, num=500)
+y = np.zeros(len(x))
+counter = 0
+for xi in x:
+    y[counter] = xi**(K/2-1)*np.exp(-xi)
+    counter += 1
+plt.figure(1)
+plt.clf()
+plt.plot(x, y)
+plt.fill_between(x, y)
+plt.xlabel('x', fontsize=13)
+plt.ylabel('$y = x^{k/2 - 1}e^{-x}$', fontsize=13)
+plt.xticks(fontsize=10)
+plt.yticks(fontsize=10)
+plt.show()
+
+#codigo para graficar el integrando modificado de Gamma
+x = np.linspace(0, 1)
+y = np.zeros(len(x))
+counter = 0
+for xi in x:
+    y[counter] = Gamma_du(K/2, xi)
+    counter += 1
+plt.figure(2)
+plt.clf()
+plt.plot(x, y)
+plt.xlabel('x', fontsize=13)
+plt.ylabel(r'$y = (-\ln (x))^{K/2 - 1}$', fontsize=13)
+plt.fill_between(x, y)
+plt.xticks(fontsize=10)
+plt.yticks(fontsize=10)
+plt.show()
+
+#codigo para graficar Gamma
+x = np.linspace(1, 3)
+y = np.zeros(len(x))
+counter = 0
+for xi in x:
+    y[counter] = Gamma(xi, dx = 1e-3)
+    counter += 1
+plt.figure(3)
+plt.clf()
+plt.plot(x, y)
+plt.xticks(fontsize=10)
+plt.yticks(fontsize=10)
+plt.show()
+
+#graficos de tolerancias comentados porque demoran mucho
+
+#codigo para graficar distintas tolerancias relativas y determinar el valor a utilizar 
+tolerancias_de_prueba = np.logspace(-6, -1)
+error = np.zeros(len(tolerancias_de_prueba))
+
+plt.clf()
+
+counter = 0
+for tol in tolerancias_de_prueba:
+    dif1 = np.abs(Gamma(1, dx = tol) - 1) #Gamma(1) = 1
+    dif2 = np.abs(Gamma(2, dx = tol) - 1) #Gamma(2) = 1
+    dif3 = np.abs(Gamma(3, dx = tol) - 2) #Gamma(3) = 2
+    dif4 = np.abs(Gamma(4, dx = tol) - 6) #Gamma(4) = 6
+    error[counter] = np.mean(np.array([dif1, dif2, dif3, dif4]))
+    counter += 1
+plt.figure(4)
+plt.plot(tolerancias_de_prueba, error)
+plt.xscale('log')
+plt.yscale('log')
+plt.xlabel('Tolerancia relativa para Simpson', fontsize=13)
+plt.ylabel('Promedio de errores absolutos cometido', fontsize=13)
+plt.xticks(fontsize=10)
+plt.yticks(fontsize=10)
+plt.show()
+
+tolerancias_de_prueba_2 = np.logspace(-12, -2)
+error_2 = np.zeros(len(tolerancias_de_prueba_2))
+
+plt.clf()
+
+counter = 0
+for tol_2 in tolerancias_de_prueba_2:
+    a = biseccion(prob_menos_95, K, 5, 15, tol_abs=tol_2)
+    dif = np.abs(prob_menos_95(K, a3))
+    error_2[counter] = dif
+    counter += 1
+plt.figure(5)
+plt.plot(tolerancias_de_prueba_2, error_2)
+plt.xscale('log')
+plt.yscale('log')
+plt.xlabel('Tolerancia absoluta para bisección', fontsize=13)
+plt.ylabel('Error absoluto cometido', fontsize=13)
+plt.xticks(fontsize=10)
+plt.yticks(fontsize=10)
+plt.show()
+
+
+#finalmente se graficará la forma de la función chi2, así como el valor a obtenido
+x = np.linspace(0, 1.1*a3)
+y = chi2(K, x)
+
+x1 = np.linspace(0, a3)
+y1 = chi2(K, x1)
+
+plt.figure(6)
+plt.clf()
+plt.title(r'$\chi ^2 (x)$ para $k=4.495$')
+plt.xlabel('x', fontsize=13)
+plt.ylabel(r'y = $\chi ^2 (x)$', fontsize=13)
+plt.axvline(a3, label = 'a = {}'.format(a3), color = 'r')
+plt.fill_between(x1, 0, y1, label = r'$P(x < a) = \int_0^a \chi^2 (x) dx$')
+plt.plot(x, y, label=r'$\chi^2(x)$')
+plt.xticks(fontsize=10)
+plt.yticks(fontsize=10)
+plt.legend()
+plt.show()
+
+x=np.linspace(0,20)
+y=np.zeros(len(x))
+i=0
+for xi in x:
+    y[i]=prob_chi2(K, xi)
+    i+=1
+plt.figure(7)
+plt.plot(x,y, color='green')
+plt.xlabel('x', fontsize=13)
+plt.ylabel(r'y=$\int_0^x \chi^2(t)dt$', fontsize=13)
+plt.xticks(fontsize=10)
+plt.yticks(fontsize=10)
+plt.axhline(y=0.95, color='r', label='y=0.95')
+plt.axvline(x=a3, color='b', label='x={}'.format(a3))
+plt.legend()
+plt.show()
--- Codigo/prob.py
+++ Codigo/prob.py
@@ -1,25 +1,17 @@
 import numpy as np
-import matplotlib.pyplot as plt
-
-def pto_medio(func, z, a, b):
-    dx = b-a
-    med = (a+b)/2
-    rec = func(z, med)
-    return rec*dx
-
-def trapecio(func, z, a, b, N = 100):
-    dx = (b-a)/(N)
-    f0 = func(z, a)
-    fN = func(z, b)
-
-    sum = (f0 + fN)/2
-    for i in range(1,N):
-        sum += func(z, a+i*dx)
-    sum *= dx
-    return sum
-
 
 def simpson(func, z, a, b, N = 100, rel_tol = 1e-6):
+    def trapecio(func, z, a, b, N = 100):
+        dx = (b-a)/(N)
+        f0 = func(z, a)
+        fN = func(z, b)
+
+        sum = (f0 + fN)/2
+        for i in range(1,N):
+            sum += func(z, a+i*dx)
+        sum *= dx
+        return sum
+
     dif = np.infty #primera vez siempre entra al bucle
     
     while dif > rel_tol:
@@ -36,6 +28,11 @@ def Gamma_du(z, u): #integrando de la funcion gamma con u=e^(-x)
     return gdu
 
 def Gamma(z, dx = 1e-6):
+    def pto_medio(func, z, a, b):
+        dx = b-a
+        med = (a+b)/2
+        rec = func(z, med)
+        return rec*dx
     #nuevos extremos gracias al C.V.
     a = 0
     b = 1
@@ -44,23 +41,23 @@ def Gamma(z, dx = 1e-6):
     return g
 
 K = 4.495 #rut 20299495-4
-gamma_k_medios = Gamma(K/2)
+GAMMA_K_MEDIOS = Gamma(K/2, dx=1e-8)
 
 def chi2(k, x):
-    res = 1/(np.exp2(k/2)*gamma_k_medios)*x**(k/2 - 1)*np.exp(-x/2) #definicion de chi^2(x)
+    res = 1/(np.exp2(k/2)*GAMMA_K_MEDIOS)*x**(k/2 - 1)*np.exp(-x/2) #definicion de chi^2(x)
     return res
 
 #calculo de int_0^a chi^2(x) dx, x<0 no es parte del dominio de chi^2
-def prob_chi2(k, a):
-    prob = simpson(chi2, k, 0, a)
+def prob_chi2(k, a, rel_tol=1e-6):
+    prob = simpson(chi2, k, 0, a, rel_tol=rel_tol)
     return prob
 
 #el problema prob_chi2(a) = 0.95 se puede reescribir como el problema del cero de una funcion
 # definiendo g(a) = prob_chi2(a) - 0.95 = 0
-def biseccion(func, k, a, b, tol=1e-6, lim=1e6): #lim permite evitar que el programa colapse 
+def biseccion(func, k, a, b, tol_abs=1e-6, lim=1e6): #lim permite evitar que el programa colapse 
     counter = 0
     dif = np.abs(func(k, b)-func(k, a))
-    while  dif > tol:
+    while  dif > tol_abs:
         dif = np.abs(func(k, b)-func(k, a))
         p = (a+b)/2
         if counter > lim: 
@@ -77,107 +74,26 @@ def biseccion(func, k, a, b, tol=1e-6, lim=1e6): #lim permite evitar que el prog
         counter += 1
     return p
 
-def prob_menos_95(k, a):
-    res = prob_chi2(k, a) - 0.95 #se quiere encontrar res = 0
+def prob_menos_95(k, a, rel_tol=1e-8):
+    res = prob_chi2(k, a, rel_tol=rel_tol) - 0.95 #se quiere encontrar res = 0
     return res
 
-#codigo para graficar el integrando original de Gamma
-""" x = np.linspace(0, 10, num=500)
-y = np.zeros(len(x))
-counter = 0
-for xi in x:
-    y[counter] = xi**(K/2-1)*np.exp(-xi)
-    counter += 1
-plt.figure(1)
-plt.clf()
-plt.plot(x, y)
-plt.fill_between(x, y)
-plt.xlabel('x')
-plt.ylabel('$y = x^{k/2 - 1}e^{-x}$', fontsize='x-large')
-plt.show() """
+def newton(func, derivada, k, x_0, tol_abs = 1e-6):
+    #f(x) = \int_0^x chi^2(t)dt - 0.95
+    #f'(x) = chi^2(x)
+    #x_{i+1} = x_i - f(x_i)/f'(x_i)
+    dif = np.infty
+    while dif > tol_abs:
+        x_1 = x_0 - func(k, x_0)/derivada(k, x_0)
+        dif = np.abs(func(k, x_1)-func(k, x_0))
+        x_0 = x_1
+    return x_1
 
-#codigo para graficar el integrando modificado de Gamma
-""" x = np.linspace(0, 1)
-y = np.zeros(len(x))
-counter = 0
-for xi in x:
-    y[counter] = Gamma_du(K/2, xi)
-    counter += 1
-plt.figure(2)
-plt.clf()
-plt.plot(x, y)
-plt.xlabel('x')
-plt.ylabel(r'$y = (-\ln (x))^{K/2 - 1}$', fontsize='x-large')
-plt.fill_between(x, y)
-plt.show() """
-
-#codigo para graficar Gamma
-""" x = np.linspace(1, 3)
-y = np.zeros(len(x))
-counter = 0
-for xi in x:
-    y[counter] = Gamma(xi, dx = 1e-3)
-    counter += 1
-plt.clf()
-plt.plot(x, y)
-plt.show()
- """
-
-#codigo para graficar distintas tolerancias relativas y determinar el valor a utilizar 
-""" tolerancias_de_prueba = np.logspace(-6, -1)
-error = np.zeros(len(tolerancias_de_prueba))
-
-plt.clf()
-
-counter = 0
-for tol in tolerancias_de_prueba:
-    dif1 = np.abs(Gamma(1, dx = tol) - 1) #Gamma(1) = 1
-    dif2 = np.abs(Gamma(2, dx = tol) - 1) #Gamma(2) = 1
-    dif3 = np.abs(Gamma(3, dx = tol) - 2) #Gamma(3) = 2
-    dif4 = np.abs(Gamma(4, dx = tol) - 6) #Gamma(4) = 6
-    error[counter] = np.mean(np.array([dif1, dif2, dif3, dif4]))
-    counter += 1
-plt.plot(tolerancias_de_prueba, error)
-plt.xscale('log')
-plt.yscale('log')
-plt.xlabel('tolerancia relativa para Simpson')
-plt.ylabel('promedio de errores absolutos cometido')
-plt.show() """
-
-""" tolerancias_de_prueba_2 = np.logspace(-12, -6)
-error_2 = np.zeros(len(tolerancias_de_prueba_2))
-
-plt.clf()
-
-counter = 0
-for tol_2 in tolerancias_de_prueba_2:
-    a = biseccion(prob_menos_95, K, 5, 15, tol=tol_2)
-    dif = np.abs(prob_menos_95(K, a))
-    error_2[counter] = dif
-    counter += 1
-
-plt.plot(tolerancias_de_prueba_2, error_2)
-plt.xscale('log')
-plt.yscale('log')
-plt.xlabel('tolerancia absoluta para bisección')
-plt.ylabel('error absoluto cometido')
-plt.show() """
-
-#a = biseccion(prob_menos_95, K, 5, 15, tol=1e-12) #a debiera ser cercano a 10.28
-
-#finalmente se graficará la forma de la función chi2, así como el valor a obtenido
-""" x = np.linspace(0, 1.1*a)
-y = chi2(K ,x)
-
-x1 = np.linspace(0, a)
-y1 = chi2(K, x1)
-
-plt.clf()
-plt.title(r'$\chi ^2 (x)$ para $k=4.495$')
-plt.xlabel('x')
-plt.ylabel(r'y = $\chi ^2 (x)$')
-plt.axvline(a, label = 'a = {}'.format(a), color = 'r')
-plt.fill_between(x1, 0, y1, label = r'$P(x < a) = \int_0^a \chi^2 (x) dx$')
-plt.plot(x, y, label=r'$\chi^2(x)$')
-plt.legend()
-plt.show() """
+tol = 1e-5
+a1 = biseccion(prob_menos_95, K, 0, 20, tol_abs=tol)
+a2 = newton(prob_menos_95, chi2, K, 1, tol_abs=tol)
+print('a por Biseccion:', a1)
+print('a por Newton:', a2)
+print('Diferencia:', np.abs(a1-a2))
+a3 = newton(prob_menos_95, chi2, K, a1, tol_abs=1e-12)
+print('a por Newton dando como punto inicial el de biseccion: ', a3)
